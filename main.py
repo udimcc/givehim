0e079cdddab9c2e5a95c93200f00b8761e175d41
Roei Yehuda <roei.yehuda@com>
2021-02-25 19:28:22 +0200

    added basic python files

---
+++ main.py
@@ -0,0 +1,8 @@
+# this file should be the main file that will be the final solution
+
+def main():
+	print("Started running ...")
+
+
+if __name__ == '__main__':
+	main()
---
+++ test.py
@@ -0,0 +1,8 @@
+# this file should use you for free testing dont upload it. play with it
+
+def main():
+	print("Started running ...")
+
+
+if __name__ == '__main__':
+	main()
