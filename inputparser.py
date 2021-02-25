c3911782ba421bd871cecf4ea9928b7703e245cf
Tom Porat <tomporat247@gmail.com>
2021-02-25 21:33:41 +0200

    Updated input parser

--- inputparser.py
+++ inputparser.py
@@ -1,6 +1,6 @@
 # %%
 from car import Car
-from path import Path
+from street import Street
 from dataclasses import dataclass
 from typing import List
 
@@ -8,12 +8,12 @@ def get_input(file_path):
 	with open(file_path) as f:
 		deadline, num_intersections, num_streets, num_cars, fixed_bonus_points = f.readline().split()
 
-		paths = []
+		streets = []
 
 		for i in range(int(num_streets)):
 			start_intersection, end_intersection, street_name, length = f.readline().split()
 
-			paths.append(Path(int(start_intersection), int(end_intersection), street_name, int(length)))
+			streets.append(Street(int(start_intersection), int(end_intersection), street_name, int(length)))
 
 		car_paths = []
 
@@ -23,7 +23,7 @@ def get_input(file_path):
 			car_paths.append(Car(street_names))
 		
 	return InputCluster(int(deadline), int(num_intersections), int(num_streets), int(num_cars), int(fixed_bonus_points),
-						paths, car_paths)
+						streets, car_paths)
 
 
 @dataclass
@@ -33,7 +33,7 @@ class InputCluster:
 	num_streets : int
 	num_cars : int
 	fixed_bonus_points : int
-	paths : List[Path]
+	streets : List[Street]
 	car_paths : List[Car]
 
 # %%
